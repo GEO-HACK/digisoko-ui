bf5bc333d21e00cbfe9e424ddc53622e9cbf3dcc
GEO-HACK <geoffreykirumba0@gmail.com>
2024-09-19 14:04:53 +0300

    navigation bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,20 +4,23 @@ import {Link,   NavLink } from 'react-router-dom'
 export default function Navbar(){
     return (
 
-        <header className='navbar'>
-            <Link className='logo' to='/'>DigiSoko</Link>
+        <header className='flex items-center bg-lime-300 h-15 py-5 px-4'>
+            <Link className='text-black text-2xl font-extrabold uppercase italic mr-auto' to='/'>DigiSoko</Link>
 
-            <nav>
+            <nav className='flex space-x-4'>
                 <NavLink 
                 to='/'
+                className="text-gray-700 font-semibold px-4 hover:text-black"
                 
                 >Home</NavLink>
                 <NavLink 
                 to='/products'
+                className="text-gray-700 font-semibold px-4 hover:text-black"
                 >products</NavLink>
                 
                 <NavLink 
                 to='/about'
+                className="text-gray-700 font-semibold px-4 hover:text-black"
                 
                 >About</NavLink>
             </nav>
