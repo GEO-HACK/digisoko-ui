52005d06017ced75d7d9528f00dcf6f232c39796
GEO-HACK <geoffreykirumba0@gmail.com>
2024-12-16 17:35:29 +0300

    about us section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
-import Card from "../components/productCard";
+// import Card from "../components/productCard";
 import Hero from "../components/Hero";
 import Choice from "../components/choose";
 import HomeProd from "../components/homeprod";
+import image1 from "../assets/images/vision.jpg";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Home() {
+  const navigate= useNavigate()
+  const GoToAbout =()=> {
+    navigate('/about')
+    window.scrollTo(0, 0);
+  }
   const [products, setProduct] = useState([]);
 
   useEffect(() => {
@@ -25,6 +33,24 @@ export default function Home() {
         <Hero />
 
         <HomeProd />
+
+        <div className="flex gap-4 bg-lime-500">
+          <div className="flex-1  ">
+            <img src={image1} alt=" About us image" className=" p-6" />
+          </div>
+          <div className="flex-1 flex flex-col items-center m-auto gap-4 text-gray-100">
+            <h1 className="text-6xl font-bold ">About Us</h1>
+            <p className="text-md font-semibold">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              tempore corrupti error fugit, dolore explicabo quae mollitia
+              temporibus cupiditate corporis aut. Molestias, numquam quia quam
+              obcaecati nesciunt eveniet porro possimus.
+            </p>
+            <button
+            onClick={GoToAbout}
+             className="px-5 py-2 rounded-lg border border-lime-100 bg-lime-50 text-lime-400 font-bold hover:bg-transparent">Learn More</button>
+          </div>
+        </div>
 
         <Choice />
       </div>
